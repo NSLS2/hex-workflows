d386f9ca514347ccd1e872bb4c972212195b0c2b
Max Rakitin <mrakitin@bnl.gov>
2024-03-29 16:58:31 -0400

    Minor tweaks

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,7 +1,7 @@
 from prefect import task, flow, get_run_logger
 from data_validation import data_validation
-from nx_exporter_edxd import export_flow
-
+from nx_exporter_edxd import export_flow as export_edxd_flow
+from nx_exporter_tomo import export_tomo_flow
 
 @task
 def log_completion():
@@ -12,7 +12,8 @@ def log_completion():
 @flow
 def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
-    export_flow(uid)
+    # export_edxd_flow(uid)
+    export_tomo_flow(uid)
     data_validation(uid)
     log_completion()
 
