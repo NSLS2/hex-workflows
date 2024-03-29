a08fdd2bc00b7afe91c0aa00d4fda589beae8db2
Max Rakitin <mrakitin@bnl.gov>
2024-03-29 17:55:03 -0400

    Exclude edxd exporting for now

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,6 +1,6 @@
 from prefect import task, flow, get_run_logger
 from data_validation import data_validation
-from nx_exporter_edxd import export_flow as export_edxd_flow
+# from nx_exporter_edxd import export_flow as export_edxd_flow
 from nx_exporter_tomo import export_tomo_flow
 
 @task
