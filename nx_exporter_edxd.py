06e5f070a11d7324a820ff24a85978aed8d5ee54
Abigail Giles <agiles@bnl.gov>
2024-05-20 17:13:26 -0400

    Add export_dir if not in start doc

--- nx_exporter_edxd.py
+++ nx_exporter_edxd.py
@@ -30,12 +30,22 @@ def export(ref, det_name="GeRM"):
     # Get the BlueskyRun from Tiled.
     run = tiled_client_hex[ref]
 
-    if run.metadata["start"].get("theta") is not None:
+    start_doc = run.metadata["start"]
+
+    if start_doc.get("theta") is not None:
         file_prefix = None
     else:
         file_prefix = "scan_{start[scan_id]:05d}_{start[calibrant]}_{date.month:02d}_{date.day:02d}_{date.year:04d}.nxs"
 
-    nx_file_path = nx_export(run, det_name, file_prefix=file_prefix)
+    # TODO: commissioning may need to be replaced by cycle after testing
+    # /nsls2/data/hex/proposals/commissioning/pass-315258/exported_data/
+    # NOTE: exported_data is folder for automated exporting
+    if start_doc.get("export_dir") is not None:
+        export_dir = None
+    else:
+        export_dir = f"/nsls2/data/hex/proposals/comissioning/{start_doc.get('data_session')}/exported_data/"
+
+    nx_file_path = nx_export(run, det_name, file_prefix=file_prefix, export_dir=export_dir)
     logger.info(f"Exported file: {nx_file_path}")
     return nx_file_path
 
