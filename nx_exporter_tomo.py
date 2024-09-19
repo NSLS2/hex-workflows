af5fd9fa276b7217f227ea786ca5014c02700946
Abigail Giles <agiles@bnl.gov>
2024-09-19 13:13:34 -0400

    Make relative paths actual paths

--- nx_exporter_tomo.py
+++ nx_exporter_tomo.py
@@ -65,10 +65,10 @@ def export_tomo(run, export_dir=None):
     common_parent_dir = os.path.commonprefix([export_dir, panda_filepath])
     print(f"{common_parent_dir = }")
 
-    rel_panda_filepath = f"../../{Path(panda_filepath).relative_to(common_parent_dir)}"
+    rel_panda_filepath = Path(f"../../{Path(panda_filepath).relative_to(common_parent_dir)}")
     rel_det_filepaths = {}
     for stream_name, det_filepath in det_filepaths.items():
-        rel_det_filepaths[stream_name] = f"../../{Path(det_filepath).relative_to(common_parent_dir)}"
+        rel_det_filepaths[stream_name] = Path(f"../../{Path(det_filepath).relative_to(common_parent_dir)}")
 
     print(f"{rel_panda_filepath = }")
     print(f"{rel_det_filepaths = }")
