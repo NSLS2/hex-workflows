c8125732b4a510ef2a256091e5abdaed51627d4b
Jun Aishima <jaishima@bnl.gov>
2025-09-16 14:11:34 -0400

    remove unnecessary comment, fix deployment name

--- run-deployment.py
+++ run-deployment.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 from prefect.deployments import run_deployment
 
 run_deployment(
-    name="end-of-run-workflow/hex-workflow-prefect3-deploy",
-    # name="end-of-run-workflow/end_of_run_workflow_deployment",
+    name="end-of-run-workflow/hex-end-of-run-workflow-docker",
     parameters={"stop_doc": {"run_start": ""}},
     timeout=15,  # don't wait for the run to finish # edit to 15 sec
 )
