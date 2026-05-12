6fc46e3fd0c4628fa5e5d64cd748ce958379909b
Jun Aishima <jaishima@bnl.gov>
2026-05-12 08:42:09 -0400

    add slack messaging
     * mon-prefect, mon-prefect-hex, mon-prefect-hxm

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,28 +1,79 @@
 import os
-from prefect import flow, get_run_logger, task
+import traceback
 
-from data_validation import data_validation
+from prefect import flow, get_run_logger, task
+from prefect.blocks.notifications import SlackWebhook
+from prefect.context import FlowRunContext
+from prefect.settings import PREFECT_UI_URL
+
+from data_validation import data_validation, get_run, get_api_key_from_env
 from nx_exporter_edxd import export_edxd_flow
 from nx_exporter_tomo import export_tomo_flow
-from tiled.client import from_uri
-from dotenv import load_dotenv
+
+CATALOG_NAME = "hex"
 
 
-def get_api_key_from_env(api_key=None):
-    logger = get_run_logger()
-    with open("/srv/container.secret", "r") as secrets:
-        load_dotenv(stream=secrets)
-    api_key = os.environ["TILED_API_KEY"]
-    return api_key
+def slack(func):
+    """
+    Send a message to mon-prefect and mon-prefect-hxm slack channels if the flow-run failed.
+    Send a message to mon-prefect-hex slack channel with the flow-run status.
+    Send a message to mon-bluesky slack channel if the bluesky-run failed.
 
+    NOTE: the name of this inner function is the same as the real end_of_workflow() function because
+    when the decorator is used, Prefect sees the name of this inner function as the name of
+    the flow. To keep the naming of workflows consistent, the name of this inner function had to match the expected name.
+    """
 
-@task
-def get_run(uid, api_key=None):
-    tiled_client = from_uri("https://tiled.nsls2.bnl.gov", api_key=api_key)
-    run = tiled_client["hex"]["raw"][uid]
-    return run
+    def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
+        flow_run_name = FlowRunContext.get().flow_run.dict().get("name")
 
+        # Load slack credentials that are saved in Prefect.
+        mon_prefect = SlackWebhook.load("mon-prefect")
+        mon_bluesky = SlackWebhook.load("mon-bluesky")
+        mon_prefect_hex = SlackWebhook.load("mon-prefect-hex")
+        mon_prefect_hxm = SlackWebhook.load("mon-prefect-hxm")
 
+        # Get the uid.
+        uid = stop_doc["run_start"]
+
+        # Get Tiled API key, if not set already
+        if not api_key:
+            api_key = get_api_key_from_env()
+
+        # Get the scan_id.
+        run = get_run(uid, api_key=api_key)
+        scan_id = run.start["scan_id"]
+
+        # Send a message to mon-bluesky if bluesky-run failed.
+        if stop_doc.get("exit_status") == "fail":
+            mon_bluesky.notify(
+                f":bangbang: {CATALOG_NAME} bluesky-run failed. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```reason: {stop_doc.get('reason', 'none')}```"
+            )
+
+        try:
+            result = func(stop_doc, api_key=api_key, dry_run=dry_run)
+
+            # Send a message to mon-prefect-hex if flow-run is successful.
+            message = f":white_check_mark: {CATALOG_NAME} flow-run successful. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}```"
+            mon_prefect_hex.notify(message)
+            return result
+        except Exception as e:
+            tb = traceback.format_exception_only(e)
+
+            # Send a message to mon-prefect-hex, mon-prefect if flow-run failed.
+            message = f":bangbang: {CATALOG_NAME} flow-run failed. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
+            mon_prefect.notify(message)
+            mon_prefect_hex.notify(message)
+            flow_run = FlowRunContext.get().flow_run
+            # Add link to flow-run for the message to mon-prefect-hxm.
+            program_message = (
+                f":bangbang: {CATALOG_NAME} flow-run failed. <https://{PREFECT_UI_URL.value()}/flow-runs/"
+                + f"flow-run/{flow_run.id}|the flow run link> (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
+            )
+            mon_prefect_hxm.notify(program_message)
+            raise
+
+    return end_of_run_workflow
 @task
 def log_completion():
     logger = get_run_logger()
